f6434c53d7bff373ee84a647745fd5ad86c56b43
Jack <ahfhafh@gmail.com>
2022-10-03 13:09:56 -0600

    individual command files

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,13 +1,23 @@
-import { SlashCommandBuilder } from '@discordjs/builders';
-import { REST } from '@discordjs/rest';
-import { Routes } from 'discord-api-types/v9';
-import 'dotenv/config';
+const fs = require('node:fs');
+const path = require('node:path');
+const { REST, Routes } = require('discord.js');
+require('dotenv').config();
 
-const commands = [
-	new SlashCommandBuilder().setName('blo').setDescription('Replies with pong!'),
-	new SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
-	new SlashCommandBuilder().setName('user').setDescription('Replies with user info!'),
-].map(command => command.toJSON());
+// const commands = [
+// 	new SlashCommandBuilder().setName('getInfo').setDescription('Get info from wesite!'),
+// 	new SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
+// 	new SlashCommandBuilder().setName('user').setDescription('Replies with user info!'),
+// ].map(command => command.toJSON());
+
+const commands = [];
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	commands.push(command.data.toJSON());
+}
 
 const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
 
--- main.js
+++ main.js
@@ -1,39 +1,39 @@
-import { Client, Intents } from 'discord.js';
-import 'dotenv/config';
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, Collection, GatewayIntentBits } = require('discord.js');
+require('dotenv').config();
 
-const leagueBot = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.DIRECT_MESSAGES, Intents.FLAGS.GUILD_INTEGRATIONS] });
+const leagueBot = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+leagueBot.commands = new Collection();
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	// Set a new item in the Collection
+	// With the key as the command name and the value as the exported module
+	leagueBot.commands.set(command.data.name, command);
+}
 
 leagueBot.once('ready', () => {
 	leagueBot.user.setActivity('with ur mom', { type: 'PLAYING' });
 	console.log('ready');
-
-	// const guild = leagueBot.guilds.cache.get(process.env.GUILD_ID);
-	// let commands;
-
-	// if (guild) {
-	// 	commands = guild.commands;
-	// } else {
-	// 	commands = leagueBot.application?.commands;
-	// }
-
-	// commands?.create({
-	// 	name: 'splock',
-	// 	description: 'Replay with pong.',
-	// });
 });
 
 leagueBot.on('interactionCreate', async (interaction) => {
-	console.log(interaction);
-	if (!interaction.isCommand()) return;
+	if (!interaction.isChatInputCommand()) return;
 
-	const { commandName } = interaction;
+	const command = leagueBot.commands.get(interaction.commandName);
 
-	if (commandName === 'blo') {
-		await interaction.reply('Pong!');
-	} else if (commandName === 'server') {
-		await interaction.reply('Server info.');
-	} else if (commandName === 'user') {
-		await interaction.reply('User info.');
+	if (!command) return;
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
 	}
 });
 
